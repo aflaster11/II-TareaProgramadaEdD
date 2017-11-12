bb3ba48a31ab4ca12d1075d864580d0001e553c3
Ducan-Ally <Fakebian98@gmail.com>
2017-11-11 20:42:46 -0600

    Agregué mis .h

---
+++ Algoritmos.h
@@ -0,0 +1,12 @@
+#ifndef algoritmos_2017
+#define algoritmos_2017
+
+#include "GNDMatriz.h"
+
+class Algoritmos{
+	public:
+		int iguales(Grafo*);
+		Grafo*  copiar(Grafo*,Grafo*);
+}
+
+#endif
---
+++ GNDMatriz.h
@@ -0,0 +1,41 @@
+#ifndef grafonodirigidoMatriz_2017
+#define grafonodirigidoMatriz_2017
+
+class Grafo {
+    class Vertice;
+    class Arista;
+    private:
+        class Vertice{
+            public:
+                char etiq;
+                Vertice(char);
+                ~Vertice();
+        }
+        class Arista{
+            public:
+                int peso;
+                Vertice VerticeA;
+                Vertice VerticeB;
+                Arista(Vertice, Vertice, int);
+                ~Arista();
+        }
+    public:
+        Grafo();
+        ~Grafo();
+		
+        void vaciar();
+        int vacia();
+		
+        Vertice agrVertice (char);
+        void agrArista(Vertice,Vertice,int);
+        void modificarPeso(Vertice,Vertice, int);
+        void elimVertice(Vertice);
+        void elimArista(Vertice, Vertice);
+        void modificarEtiqueta(Vertice, char);
+        char etiqueta(Vertice);
+        int numVertices();
+        int numVerticesAdy();
+        int adyacentes(Vertice, Vertice)
+};
+
+#endif
